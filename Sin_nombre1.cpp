0b30337fd848b7cc3c1abcd3e4b7f8549f8825ec
andrestorresaioros <55698665+andrestorresaioros@users.noreply.github.com>
2019-09-23 07:51:53 -0500

    Add files via upload

---
+++ Sin_nombre1.cpp
@@ -0,0 +1,52 @@
+/*
+2.) Se tiene la siguiente información por cada uno de los N estudiantes de
+la Universidad:
+Edad
+Sexo (1. Masculino; 2. Femenino).
+Carrera (1. Ingeniería; 2. Otra).
+Jornada (1. Diurna; 2. Nocturna).
+Hacer un algoritmo que obtenga:
+a) Promedio de edad de los estudiantes de Contaduría.
+b) Porcentaje de hombres que estudian Ingeniería.
+c) Porcentaje de mujeres menores a 20 años de la
+Universidad.
+d) Promedio de edad de las mujeres de Ingeniería.
+e) Porcentaje de hombres mayores a 22 años que estudian
+Derecho en la noche.
+*/
+#include<iostream>
+using namespace std; 	
+class estudiante{
+	private:
+		int edad, sexo,carrera,jornada;
+	public:
+		void adquirirdatos();
+		void decirdatos();
+		void realizarporcentajes();
+		void realizarpromedio();
+};
+void estudiante::adquirirdatos(){
+	cout<<"ingrese su edad: ";
+	cin>>edad;
+	cout<<"ingrese su sexo: ";
+	
+}
+void estudiante::v2(){
+	
+}
+void estudiante::v3(){
+	
+}
+void estudiante::v4(){
+	
+}
+void estudiante::v5(){
+	
+}
+int main(){
+	int edad,sexo;
+	string carrera;
+	
+	
+	
+}
